4f7d1cc3436a6d353a0e46bd610dd73c99119713
Dilara Esmer <69076113+dilaraesmer@users.noreply.github.com>
2022-12-22 01:47:39 +0300

    Add files via upload

---
+++ 2.sql
@@ -0,0 +1,20 @@
+-- CREATE TABLE example1 (
+--       id BIGSERIAL,
+--		 first_name VARCHAR(30),
+--       last_name VARCHAR(30),
+--		 city VARCHAR(30),
+--		 state CHAR(2));
+
+-----------------------------------------------------------
+
+--INSERT INTO example1 ( first_name, last_name, city, state)
+--VALUES ('Samuel', 'Smith', 'Boston', 'MA'),
+--       ('Emma', 'Johnson', 'Seattle', 'WA'),
+--	     ('John', 'Oliver', 'New York', 'NY'),
+--	     ('Olivia', 'Brown', 'San Francisco', 'CA'),
+--	     ('Simon', 'Smith', 'Dallas', 'TX');
+
+------------------------------------------------------------
+
+SELECT * FROM example1;
+
