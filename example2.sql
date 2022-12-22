d61721273d8ded83aae36dd5c02d8f00124019f8
Dilara Esmer <69076113+dilaraesmer@users.noreply.github.com>
2022-12-22 04:31:05 +0300

    Add files via upload

---
+++ example2.sql
@@ -0,0 +1,48 @@
+-- Database: test
+
+-- DROP DATABASE test;
+
+CREATE DATABASE test
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Turkish_Turkey.1254'
+    LC_CTYPE = 'Turkish_Turkey.1254'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+	
+-- modifying tables and add a column
+
+CREATE TABLE examples (
+
+   example_id SERIAL PRIMARY KEY,
+   first_name VARCHAR(30),
+   last_name VARCHAR(30)
+);
+
+SELECT * FROM examples;
+
+ALTER TABLE examples
+ADD COLUMN email VARCHAR(50) UNIQUE;
+
+ALTER TABLE examples
+ADD COLUMN nationality VARCHAR(30),
+ADD COLUMN age INT NOT NULL;
+
+-- modify a columns data type
+
+-- ALTER TABLE tablename
+ALTER COLUMN columnname TYPE new_data_type;
+
+
+ALTER TABLE examples
+ALTER COLUMN nationality TYPE CHAR(3);
+
+SELECT * FROM examples;
+
+ALTER TABLE examples 
+ALTER COLUMN last_name TYPE VARCHAR(50),
+ALTER COLUMN email TYPE VARCHAR(80);
+
+
+
