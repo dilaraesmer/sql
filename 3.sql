8eb6eb8502ffb8b8c5cd68f8455869e07ba8659d
Dilara Esmer <69076113+dilaraesmer@users.noreply.github.com>
2022-12-23 17:05:34 +0300

    Add files via upload

--- 3.sql
+++ 3.sql
@@ -43,4 +43,52 @@ WHERE example_id = 1;
 
 SELECT * FROM examples;
 
+UPDATE examples
+SET nationality = 'CAN'
+WHERE nationality = 'USA';
 
+SELECT * FROM examples;
+
+UPDATE examples 
+SET first_name = 'James', age = 55
+WHERE example_id = 5;
+
+SELECT * FROM examples;
+
+DELETE FROM examples;
+
+INSERT INTO examples (first_name, last_name, email, nationality, age)
+VALUES ('David', 'Mitchell', 'dmitch@gmail.com', 'GBR', 43);
+
+
+SELECT * FROM examples;
+
+INSERT INTO examples (first_name, last_name, email, nationality, age)
+VALUES ('Emily', 'Watson', 'ewatson@gmail.com', 'USA', 24),('Theo', 'Scott', 'tscott@gmail.com', 'AUS', 38),
+('Emily', 'Smith', 'esmith@gmail.com', 'GBR', 29), ('Jim', 'Burr', 'jburr@gmail.com', 'USA', 54);
+
+SELECT * FROM examples;
+
+-- UPDATE DATA IN A TABLE --
+
+-- UPDATE tablename
+-- SET columnname = 'newvalue'
+-- WHERE columnname = 'value';
+
+UPDATE examples
+SET email = 'davidmitchell@gmail.com'
+WHERE example_id = 1;
+
+SELECT * FROM examples;
+
+UPDATE examples
+SET nationality = 'CAN'
+WHERE nationality = 'USA';
+
+SELECT * FROM examples;
+
+UPDATE examples 
+SET first_name = 'James', age = 55
+WHERE example_id = 5;
+
+SELECT * FROM examples;
