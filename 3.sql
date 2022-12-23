9c03fd9e73e02218287e301e1f2b32c87342b602
Dilara Esmer <69076113+dilaraesmer@users.noreply.github.com>
2022-12-23 16:23:02 +0300

    Add files via upload

--- 3.sql
+++ 3.sql
@@ -12,17 +12,21 @@ CREATE DATABASE test
     CONNECTION LIMIT = -1;
 	
 	
--- INSERT DATA INTO A TABLE
+-- INSERT DATA INTO A TABLE --
 
-/* 
 
-INSERT INTO tablename (columnname1, columnname2, columnname3)
-VALUES ('value1', 'value2', 'value3');
+-- /* INSERT INTO tablename (columnname1, columnname2, columnname3)
+-- VALUES ('value1', 'value2', 'value3'); /*
+
  
-/*
- 
-
 INSERT INTO examples (first_name, last_name, email, nationality, age)
 VALUES ('David', 'Mitchell', 'dmitch@gmail.com', 'GBR', 43);
+
+
+SELECT * FROM examples;
+
+INSERT INTO examples (first_name, last_name, email, nationality, age)
+VALUES ('Emily', 'Watson', 'ewatson@gmail.com', 'USA', 24),('Theo', 'Scott', 'tscott@gmail.com', 'AUS', 38),
+('Emily', 'Smith', 'esmith@gmail.com', 'GBR', 29), ('Jim', 'Burr', 'jburr@gmail.com', 'USA', 54);
 
 SELECT * FROM examples;
