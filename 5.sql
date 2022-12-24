2677a1a8a89291fd6b8c23dee45a5f8f25f543ba
Dilara Esmer <69076113+dilaraesmer@users.noreply.github.com>
2022-12-24 23:38:30 +0300

    Add files via upload

--- 5.sql
+++ 5.sql
@@ -26,4 +26,38 @@ WHERE columnname = 'value';
 
 */
 
-SELECT * FROM movies;
+SELECT * FROM movies
+WHERE age_certificate = '15';
+
+SELECT * FROM movies
+WHERE age_certificate = '15'
+AND movie_lang = 'Chinese';
+
+SELECT * FROM movies
+WHERE age_certificate = '15'
+OR movie_lang = 'Chinese';
+
+SELECT * FROM movies
+WHERE movie_lang = 'English'
+AND age_certificate = '15'
+AND director_id = 27;
+
+-- Using logical operators (>, >=, <, <=)
+
+SELECT movie_name, movie_length FROM movies
+WHERE movie_length > 120;
+
+SELECT movie_name, movie_length FROM movies
+WHERE movie_length >= 120;
+
+SELECT movie_name, movie_length FROM movies
+WHERE movie_length < 120;
+
+SELECT movie_name, movie_length FROM movies
+WHERE movie_length <= 120;
+
+SELECT * FROM movies
+WHERE release_date > '1999-12-31';
+
+SELECT * FROM movies
+WHERE release_date < '1999-12-31';
