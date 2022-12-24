6e12e8293d9f79cca67dcb19b60c1620f08ff2fa
Dilara Esmer <69076113+dilaraesmer@users.noreply.github.com>
2022-12-24 23:08:12 +0300

    Add files via upload

---
+++ 5.sql
@@ -0,0 +1,16 @@
+-- Database: movie_data
+
+-- DROP DATABASE movie_data;
+
+CREATE DATABASE movie_data
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Turkish_Turkey.1254'
+    LC_CTYPE = 'Turkish_Turkey.1254'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+	
+	SELECT * FROM directors;
+	
+	SELECT first_name, last_name FROM directors;
