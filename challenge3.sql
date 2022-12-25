dfdc334130a2e1f3267fdd54c7ae6eca76a2b5df
Dilara Esmer <69076113+dilaraesmer@users.noreply.github.com>
2022-12-25 17:35:47 +0300

    Add files via upload

--- challenge3.sql
+++ challenge3.sql
@@ -20,3 +20,16 @@ SELECT movie_name, release_date FROM movies;
 
 SELECT first_name, last_name FROM directors
 WHERE nationality = 'American';
+
+-- 3. Select all male actors born after the 1st of January 1970
+
+SELECT * FROM actors
+WHERE gender = 'M'
+AND date_of_birth > '1970-01-01';
+
+-- 4. Select the names of all movies which are over 90 minutes long and movie 
+-- language is English.
+
+SELECT movie_name FROM movies
+WHERE movie_length > 90
+AND movie_lang = 'English';
