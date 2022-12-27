e1ea0900323ef1d0505bf3045b15cbc91f7deb2a
Dilara Esmer <69076113+dilaraesmer@users.noreply.github.com>
2022-12-27 18:14:40 +0300

    Add files via upload

--- 6.sql
+++ 6.sql
@@ -168,9 +168,29 @@ FETCH FIRST 1 ROW ONLY;
 SELECT movie_id, movie_name FROM movies
 FETCH FIRST ROW ONLY;
 
+
 SELECT movie_id, movie_name FROM movies
 FETCH FIRST 10 ROW ONLY;
+
 
 SELECT movie_id, movie_name FROM movies
 OFFSET 8 ROWS
 FETCH FIRST 10 ROW ONLY;
+
+-- Distinct values
+
+/*
+
+SELECT DISTINCT columnname FROM tablename;
+
+*/
+
+
+SELECT DISTINCT movie_lang, age_certificate FROM movies
+ORDER BY movie_lang;
+
+SELECT DISTINCT * FROM movies
+ORDER BY movie_lang;
+
+SELECT * FROM movies;
+
