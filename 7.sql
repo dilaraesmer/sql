e6f3b2655636267e9ba7594aabcb24a388c7868e
Dilara Esmer <69076113+dilaraesmer@users.noreply.github.com>
2022-12-27 20:54:25 +0300

    Add files via upload

--- 7.sql
+++ 7.sql
@@ -61,3 +61,23 @@ SELECT CONCAT(first_name,last_name) AS full_name FROM actors;
 SELECT CONCAT(first_name,' ',last_name) AS full_name FROM actors;
 
 SELECT CONCAT_WS(' ', first_name, last_name, date_of_birth) AS full_name FROM actors;
+
+----EXAMPLE----
+
+-- 1. Return the top 3 movies with the highest international takings.
+
+SELECT * FROM movie_revenues
+WHERE international_takings IS NOT NULL
+ORDER BY international_takings DESC
+LIMIT 3;
+
+-- 2. Concatenate the first and last names of the directors, seperated by a space,
+-- and call this new column full_name.
+
+SELECT CONCAT_WS(' ', first_name, last_name) AS full_name FROM directors;
+
+-- 3. Return the actors with missing first_names or missing date_of_births.
+
+SELECT * FROM actors
+WHERE first_name IS NULL
+OR date_of_birth IS NULL;
