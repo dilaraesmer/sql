546ee874781f2d27a90281b14773aabe7f2f943f
Dilara Esmer <69076113+dilaraesmer@users.noreply.github.com>
2022-12-29 15:28:14 +0300

    Add files via upload

--- practice_1.sql
+++ practice_1.sql
@@ -1,6 +1,22 @@
 CREATE TABLE student (
+student_id SERIAL,
 first_name VARCHAR(50),
 last_name VARCHAR(50),
-gender VARCHAR(6), 
-date_of_birth DATE
-);
+age INTEGER,
+gender VARCHAR(7));
+DROP TABLE student;
+
+INSERT INTO student (first_name, last_name, age, gender)
+VALUES ('Anne', 'Jones', 21, 'Female'),
+('Ben', 'Smith', 23, 'Male'),
+('Rachel', 'Rey', 25, 'Female'),
+('Samuray', 'Jacob', 34, 'Male'),
+('Emine', 'Saygın', 56, 'Female'),
+('Piraye', 'Çelik', 49, 'Female'),
+('Sumru', 'Aydın', 20, 'Female'),
+('Levent', 'Esmer', 53, 'Male'), 
+('Berk', 'Ulu', 37, 'Male'),
+('Duru', 'Akın', 27, 'Female');
+
+SELECT * FROM student;
+
