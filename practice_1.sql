4e4bcc03b8272549e210f51ef99277fac91ddbf8
Dilara Esmer <69076113+dilaraesmer@users.noreply.github.com>
2022-12-29 15:57:43 +0300

    Add files via upload

--- practice_1.sql
+++ practice_1.sql
@@ -1,22 +1,28 @@
-CREATE TABLE student (
-student_id SERIAL,
+CREATE TABLE members (
+member_id SERIAL,
 first_name VARCHAR(50),
 last_name VARCHAR(50),
 age INTEGER,
-gender VARCHAR(7));
-DROP TABLE student;
+gender VARCHAR(7),
+job VARCHAR(50)
+);
 
-INSERT INTO student (first_name, last_name, age, gender)
-VALUES ('Anne', 'Jones', 21, 'Female'),
-('Ben', 'Smith', 23, 'Male'),
-('Rachel', 'Rey', 25, 'Female'),
-('Samuray', 'Jacob', 34, 'Male'),
-('Emine', 'Saygın', 56, 'Female'),
-('Piraye', 'Çelik', 49, 'Female'),
-('Sumru', 'Aydın', 20, 'Female'),
-('Levent', 'Esmer', 53, 'Male'), 
-('Berk', 'Ulu', 37, 'Male'),
-('Duru', 'Akın', 27, 'Female');
 
-SELECT * FROM student;
+INSERT INTO members (first_name, last_name, age, gender, job)
+VALUES ('Anne', 'Jones', 21, 'Female', 'Student'),
+('Ben', 'Smith', 23, 'Male', 'Teacher'),
+('Rachel', 'Rey', 25, 'Female', 'Developer'),
+('Samuray', 'Jacob', 34, 'Male', 'Pianist'),
+('Emine', 'Saygın', 56, 'Female', 'Chemist'),
+('Piraye', 'Çelik', 49, 'Female', 'Biologist'),
+('Sumru', 'Aydın', 70, 'Female', 'Philosopher'),
+('Levent', 'Esmer', 53, 'Male', 'Professional Tourist Guide'), 
+('Berk', 'Ulu', 37, 'Male', 'Football Player'),
+('Duru', 'Akın', 27, 'Female', 'Artist');
+
+SELECT * FROM members;
+
+SELECT first_name, last_name, job FROM members
+WHERE first_name = 'Levent';
+
 
