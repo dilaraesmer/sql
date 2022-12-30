b64227cbb9c6186aa0b5294c30a6c89a063dcb60
Dilara Esmer <69076113+dilaraesmer@users.noreply.github.com>
2022-12-30 04:09:22 +0300

    Add files via upload

--- 8.sql
+++ 8.sql
@@ -7,3 +7,21 @@ SELECT COUNT(columnname) FROM tablename;
 SELECT COUNT(*) FROM movie_revenues;
 
 SELECT COUNT(international_takings) FROM movie_revenues;
+
+SELECT COUNT(*) FROM movies
+WHERE movie_lang = 'English';
+
+-- Aggregate Functions : SUM
+
+/*
+SELECT SUM(columnname) FROM tablename;
+*/
+
+SELECT SUM(domestic_takings) FROM movie_revenues;
+
+
+SELECT SUM(domestic_takings) FROM movie_revenues
+WHERE domestic_takings > 100.0;
+
+SELECT SUM(movie_length) FROM movies
+WHERE movie_lang = 'Chinese';
