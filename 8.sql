92d3727ab672db982d59a104bdf00ec0f6a7a9b1
Dilara Esmer <69076113+dilaraesmer@users.noreply.github.com>
2022-12-30 04:22:01 +0300

    Add files via upload

--- 8.sql
+++ 8.sql
@@ -36,11 +36,16 @@ SELECT MIN(columnname) FROM tablename;
 SELECT MAX(movie_length) FROM movies;
 SELECT MIN(movie_length) FROM movies;
 
+
 SELECT MIN(movie_length) FROM movies
 WHERE movie_lang = 'Japanese';
 
-SELECT MAX(release_date) FROM movies;
 
+SELECT MAX(release_date) FROM movies;
 SELECT MIN(release_date) FROM movies;
 
+
 SELECT MAX(movie_name) FROM movies;
+SELECT MIN(movie_name) FROM movies;
+
+
