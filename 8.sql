c152b57174e9cca116a61ee22451d050a0f3ddc9
Dilara Esmer <69076113+dilaraesmer@users.noreply.github.com>
2023-01-01 20:01:36 +0300

    Add files via upload

--- 8.sql
+++ 8.sql
@@ -99,6 +99,15 @@ SELECT column1, AGGFUN(column1) FROM tablename
 GROUP BY column1
 HAVING AGGFUN(column3) = value;
 */
+ 
+SELECT movie_lang, COUNT(movie_lang) FROM movies
+GROUP BY movie_lang
+HAVING COUNT(movie_lang) > 1;
+
+SELECT movie_lang, COUNT(movie_lang) FROM movies
+WHERE movie_length > 120 
+GROUP BY movie_lang
+HAVING COUNT(movie_lang) > 1;
 
 
 
