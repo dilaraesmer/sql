ef59db10ff2ce5d46d9900bb98543b8270880b2c
Dilara Esmer <69076113+dilaraesmer@users.noreply.github.com>
2023-01-02 22:04:56 +0300

    Add files via upload

--- training.sql
+++ training.sql
@@ -2,5 +2,16 @@ CREATE TABLE Customer_table(
 Cust_id INT,
    first_name VARCHAR,
    last_name VARCHAR,
-   age INT);
-   
+   age INT,
+   email_id VARCHAR);
+
+
+INSERT INTO Customer_table VALUES(
+1, 'jacky', 'boston', 18, 'rsf@hgh.com');
+
+SELECT * FROM Customer_table;
+
+INSERT INTO Customer_table(
+cust_id, first_name, age, email_id)
+VALUES(2, 'dee', 21, 'ndbj@kd.com');
+
