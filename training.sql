e696f5f521c62bb506dff98d0c85b17374f4be45
Dilara Esmer <69076113+dilaraesmer@users.noreply.github.com>
2023-01-02 22:33:12 +0300

    Add files via upload

--- training.sql
+++ training.sql
@@ -15,3 +15,10 @@ INSERT INTO Customer_table(
 cust_id, first_name, age, email_id)
 VALUES(2, 'dee', 21, 'ndbj@kd.com');
 
+INSERT INTO customer_table 
+VALUES(4, 'hsbd', 'sjd', 22, 'dfj@nbd@j.com'),
+(4, 'dkn', 'dkj', 22, 'dmn@nbd@j.com');
+
+SELECT * FROM customer_table;
+
+COPY Customer_table FROM 'C:\Users\DELL\Desktop\New Folder\yeni\original.csv' DELIMITER ',' CSV HEADER;
