2f4cf8e22ce36c9b34484aab82b74e5f9a66d7e3
Dilara Esmer <69076113+dilaraesmer@users.noreply.github.com>
2023-01-04 23:30:41 +0300

    Add files via upload

---
+++ DB_csv.sql
@@ -0,0 +1,4 @@
+CREATE TABLE csv_table(Invocie ID BIGSERIAL NOT NULL, Branch CHAR, City VARCHAR(50), Customer type VARCHAR(50),
+					  Gender VARCHAR(6), Product line VARCHAR(50), Unit price numeric, Quantity real, Tax 5% real,
+					  Total real, Date DATE, Time TIME, Payment VARCHAR(50), cogs numeric, gross margin percentage real, 
+					  gross income real, Rating numeric);
